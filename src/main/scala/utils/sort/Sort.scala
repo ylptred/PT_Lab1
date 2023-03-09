8c82474f650f470db15c1a0227a5e0fda8f264d4
Danila Smirnov <ylptred@gmail.com>
2023-03-09 14:58:51 +0300

    class fix

--- src/main/scala/utils/sort/Sort.scala
+++ src/main/scala/utils/sort/Sort.scala
@@ -17,17 +17,12 @@ object Sort {
 
   def insertion_sort(arr: Array[Data]): Array[Data] = {
     for (i <- 1 until arr.length) {
-      // A[ i ] is added in the sorted sequence A[0, .. i-1]
-      // save A[i] to make a hole at index iHole
       val item = arr(i)
       var iHole = i
-      // keep moving the hole to next smaller index until A[iHole - 1] is <= item
       while (iHole > 0 && arr(iHole - 1) > item) {
-        // move hole to next smaller index
         arr(iHole) = arr(iHole - 1)
         iHole = iHole - 1
       }
-      // put item in the hole
       arr(iHole) = item
     }
     arr
@@ -49,7 +44,6 @@ object Sort {
       if (swapped) {
         swapped = false
         for (j <- arr.length - 2 to 0 by -1) if (arr(j) > arr(j + 1)) swap(j)
-        //if no elements have been swapped, then the list is sorted
       }
     } while (swapped)
     arr
